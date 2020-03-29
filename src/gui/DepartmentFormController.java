b9322f6165b98a35dfd0c1abbe4bd9af575e9549
Lucas Vieira <vieraf1@hotmail.com.br>
2020-03-28 22:52:08 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DBException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.service.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 	
 	private Department department;
+	
+	private DepartmentService service;
 
 	@FXML
 	private TextField txtId;
@@ -34,14 +42,38 @@ public class DepartmentFormController implements Initializable {
 		this.department = department;
 	}
 	
-	@FXML
-	private void onBtSaveAction() {
-		System.out.println("OnbtSave");
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@FXML
-	private void onBtSaveCancel() {
-		System.out.println("OnBtCancel");
+	private void onBtSaveAction(ActionEvent event) {
+		
+		if(department == null) {
+			throw new IllegalStateException("Department was null!");
+		}
+		if(service == null) {
+		    throw new IllegalStateException("Service was null!");
+		}
+		
+		try {
+			department = getFormData();
+			service.saveOrUpdate(department);
+			Utils.currentStage(event).close();
+		}catch(DBException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private Department getFormData() {
+		Integer id = Utils.tryParseInt(txtId.getText());
+		String name = txtName.getText();
+		return new Department(id, name);
+	}
+
+	@FXML
+	private void onBtSaveCancel(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	private void initializeNodes() {
